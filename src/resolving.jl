e61422bf69e2706c236f85259994baa5f74b1d20
PierreBarrat <p.barrat@live.fr>
2021-02-24 14:46:16 +0100

    Add resolve(trees::Dict, splits::Dict) - edit resolve!

--- src/resolving.jl
+++ src/resolving.jl
@@ -4,7 +4,7 @@
 Add splits in `S` to `t` by introducing internal nodes. 
 If `conflict != :ignore`, will fail if a split `s` in `S` is not compatible with `t`. 
 """
-function resolve!(t::Tree{T}, S::SplitList; conflict=:ignore, usemask=false, tau=0.) where T
+function resolve!(t::Tree{T}, S::SplitList; conflict=:ignore, usemask=false, tau=0., warn=true) where T
 	# Label for created nodes
 	label_i = parse(Int64, create_label(t, "RESOLVED")[10:end])
 	#
@@ -26,10 +26,26 @@ function resolve!(t::Tree{T}, S::SplitList; conflict=:ignore, usemask=false, tau
 					graftnode!(nr,r)
 				end
 				graftnode!(R, nr, tau=tau)
+				push!(tsplits.splits, s)
 			elseif conflict
 				@error "Conflicting splits"
 			end
+		else
+			@warn "Split #$i already in tree. Maybe splits in `S` are not unique?"
 		end
 	end
 	node2tree!(t, t.root)
+end
+
+"""
+	resolve(trees::Dict{T, <:Tree}, splits::Dict{T, <:SplitList}; kwargs...) where T
+
+Resolve `trees[s]` with splits in `splits[s]` by calling `TreeTools.resolve!`. `trees` and `splits` must share keys. 
+"""
+function resolve(trees::Dict{T, <:Tree}, splits::Dict{T, <:SplitList}; kwargs...) where T
+	resolved_trees = deepcopy(trees)
+	for (s,S) in splits
+		TreeTools.resolve!(resolved_trees[s], S; kwargs...)
+	end
+	return resolved_trees
 end
