38298078c6cb3a82d53c690a27ba4a2d11036295
PierreBarrat <p.barrat@live.fr>
2021-04-16 17:25:51 +0200

    mod fitch reconstruction to allow nested dict keys

--- src/sequences.jl
+++ src/sequences.jl
@@ -3,26 +3,92 @@ export fitch!
 
 """
 	fasta2tree!(tree, fastafile::String, key=:seq; warn=true)
+
+Add sequences of `fastafile` to tips of `tree`. For a leaf `n`, sequence is added to `n.data.dat[key]`. 
 """
-function fasta2tree!(tree, fastafile::String, key=:seq; warn=true)
+function fasta2tree!(tree, fastafile::String, key::Union{Symbol, AbstractString}=:seq; warn=true)
 	reader = open(FASTA.Reader, fastafile)
 	record = FASTA.Record()
 	while !eof(reader)
 	    read!(reader, record)
 	    if haskey(tree.lnodes, identifier(record))
-	    	tree.lnodes[identifier(record)].data.dat[:seq] = sequence(record)
+	    	tree.lnodes[identifier(record)].data.dat[key] = sequence(record)
 	    end
 	end
 	#
 	flag = true
 	for (name, n) in tree.lleaves
-		if !haskey(n.data.dat, :seq)
-			n.data.dat[:seq] = missing
+		if !haskey(n.data.dat, key)
+			n.data.dat[key] = missing
 			flag = false
 		end
 	end
 	warn && !flag && @warn "Not all leaves had a corresponding sequence in the alignment (file: $fastafile)."
 	return flag
+end
+
+"""
+	fasta2tree!(tree, fastafile::String, ks::Tuple; warn=true)
+
+Add sequences of `fastafile` to tips of `tree`. For a leaf `n`, sequence is added to `n.data.dat[ks[1]][ks[2]]...`. 	
+"""
+function fasta2tree!(tree, fastafile::String, ks::Tuple; warn=true)
+	# Setting dicts
+	for n in values(tree.lleaves)
+		recursive_key_init!(n.data.dat, ks[1:end-1]...)
+	end
+	key = ks[end]
+	#
+	reader = open(FASTA.Reader, fastafile)
+	record = FASTA.Record()
+	while !eof(reader)
+	    read!(reader, record)
+	    if haskey(tree.lnodes, identifier(record))
+	    	recursive_set!(tree.lnodes[identifier(record)].data.dat, sequence(record), ks...)
+	    end
+	end
+	#
+	flag = true
+	for (name, n) in tree.lleaves
+		if !recursive_haskey(n.data.dat, ks...)
+			recursive_set!(n.data.dat, missing, ks...)
+			flag = false
+		end
+	end
+	warn && !flag && @warn "Not all leaves had a corresponding sequence in the alignment (file: $fastafile)."
+	return flag
+end
+
+function recursive_key_init!(dat, key, ks...)
+	if !haskey(dat, key)
+		dat[key] = Dict()
+	end
+	recursive_key_init!(dat[key], ks...)
+end
+recursive_key_init!(dat) = nothing
+function recursive_get(dat, key, ks...)
+	if isempty(ks)
+		return dat[key]
+	else
+		return recursive_get(dat[key], ks...)
+	end
+end
+function recursive_set!(dat, value, key, ks...)
+	if isempty(ks)
+		dat[key] = value
+	else
+		recursive_set!(dat[key], value, ks...)
+	end	
+	dat
+end
+function recursive_haskey(dat, key, ks...)
+	if !haskey(dat, key)
+		return false
+	elseif isempty(ks)
+		return true
+	else
+		return recursive_haskey(dat[key], ks...)
+	end
 end
 
 
@@ -52,9 +118,9 @@ length(s::FitchState) = length(s.state)
 """
 	fitch!(t::Tree, outkey=:ancestral_seq, seqkey=:seq; clear_fitch_states=true)
 """
-function fitch!(t::Tree, outkey=:ancestral_seq, seqkey=:seq; clear_fitch_states=true)
+function fitch!(t::Tree, outkey=:seq, seqkey=:seq; clear_fitch_states=true)
 	fitchkey = :fitchstate
-	init_fitchstates!(t, fitchkey, seqkey)
+	init_fitchstates!(t, seqkey, fitchkey)
 	fitch_up!(t, fitchkey)
 	fitch_remove_gaps!(t, fitchkey)
 	fitch_root_state!(t, fitchkey)
@@ -71,9 +137,14 @@ end
 """
 	init_fitchstates!(t::Tree, fitchkey=:fitchstate, seqkey = :seq)
 """
-function init_fitchstates!(t::Tree, fitchkey=:fitchstate, seqkey = :seq)
+function init_fitchstates!(t::Tree, seqkey::Union{Symbol, AbstractString}=:seq, fitchkey=:fitchstate)
 	for n in values(t.lleaves)
 		n.data.dat[fitchkey] = FitchState(n.data.dat[seqkey])
+	end	
+end
+function init_fitchstates!(t::Tree, seqkey::Tuple, fitchkey=:fitchstate)
+	for n in values(t.lleaves)
+		n.data.dat[fitchkey] = FitchState(recursive_get(n.data.dat, seqkey...))
 	end
 end
 
@@ -175,7 +246,15 @@ end
 
 """
 """
-function fitch_sample!(t::Tree, outkey, fitchkey=:fitchstate)
+function fitch_sample!(t::Tree, outkey::Tuple, fitchkey=:fitchstate)
+	for n in values(t.lnodes)
+		if !n.isleaf
+			recursive_key_init!(n.data.dat, outkey[1:end-1]...)
+			recursive_set!(n.data.dat, fitch_sample(n.data.dat[fitchkey]), outkey...)
+		end
+	end
+end
+function fitch_sample!(t::Tree, outkey::Union{Symbol, AbstractString}, fitchkey=:fitchstate)
 	for n in values(t.lnodes)
 		if !n.isleaf
 			n.data.dat[outkey] = fitch_sample(n.data.dat[fitchkey])
