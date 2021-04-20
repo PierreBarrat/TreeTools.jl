b43212e8170f755fa5fc7f5d81ac7fc49f4e47df
PierreBarrat <p.barrat@live.fr>
2021-04-20 16:05:11 +0200

    add internal type for Mutation

--- src/mutations.jl
+++ src/mutations.jl
@@ -1,9 +1,11 @@
-struct Mutation
+export Mutation
+
+struct Mutation{T}
 	i::Int64
-	old
-	new
+	old::T
+	new::T
 end
-function Mutation(x::Tuple{Int64,Any,Any})
+function Mutation(x::Tuple{Int64,T,T} where T)
 	return Mutation(x[1],x[2],x[3])
 end
 
@@ -24,15 +26,22 @@ function parse_mutation(mut::AbstractString)
 	pos = parse(Int64, mut[2:end-1])
 	return Mutation(pos, oldstate, newstate)
 end
+function parse_mutation(mut::AbstractString, ::Val{T}) where T
+	oldstate = mut[1]
+	newstate = mut[end]
+	pos = parse(Int64, mut[2:end-1])
+	return Mutation{T}(pos, oldstate, newstate)
+end
+parse_mutation(mut::AbstractString, T) = parse_mutation(mut, Val(T))
 parse_mutation(mut::Mutation) = mut
 
 
-function compute_mutations!(f::Function, n::TreeNode, outkey)
+function compute_mutations!(f::Function, n::TreeNode, outkey, T=eltype(f(n)))
 	n.isroot && return nothing
 	n.data.dat[outkey] = Array{Mutation,1}(undef, 0)
 	for (i,a) in enumerate(f(n))
 		if a != f(n.anc)[i]
-			TreeTools.recursive_push!(n.data.dat, Mutation(i, f(n.anc)[i], a), outkey)
+			TreeTools.recursive_push!(n.data.dat, Mutation{T}(i, f(n.anc)[i], a), outkey)
 		end
 	end
 	nothing
@@ -41,10 +50,14 @@ end
 """
 	compute_mutations!(t::Tree, seqkey, outkey)
 """
+function compute_mutations!(f::Function, t::Tree, outkey, T)
+	for n in values(t.lnodes)
+		compute_mutations!(f, n, outkey, T)
+	end
+end
 function compute_mutations!(f::Function, t::Tree, outkey)
 	for n in values(t.lnodes)
 		compute_mutations!(f, n, outkey)
 	end
 end
-
 
