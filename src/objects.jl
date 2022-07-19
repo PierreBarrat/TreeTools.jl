e3f9d547b962947c3bb42c68c094ae1757e913cb
PierreBarrat <p.barrat@live.fr>
2022-07-19 15:04:33 +0200

    edit docstring MiscData

--- src/objects.jl
+++ src/objects.jl
@@ -4,7 +4,8 @@
 Abstract supertype for data attached to the `dat` field of `TreeNode` objects.
 Implemented concrete types are
 - `EmptyData`: empty struct. Use if you do not have to attach data to nodes.
-- `MiscData`: contains a dictionary for attaching extra data to nodes.
+- `MiscData`: contains a dictionary for attaching extra data to nodes. Also behaves like
+   a `Dict` for indexing/iterating, *e.g.* `x.dat[key] == x[key]`.
 """
 abstract type TreeNodeData end
 
